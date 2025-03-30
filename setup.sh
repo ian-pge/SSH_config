801ab73edf524d21ed29848e6f9f2f27d0a1b402
Ian <82505982+ian-pge@users.noreply.github.com>
2025-03-30 15:09:41 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ echo "Sourcing Nix profile..."
 . /home/zed/.nix-profile/etc/profile.d/nix.sh
 
 echo "Building flake..."
-nix build path:/workspace/.devcontainer#homeConfigurations.zed.activationPackage \
+nix build .#homeConfigurations.zed.activationPackage \
   --extra-experimental-features flakes \
   --extra-experimental-features nix-command \
   --out-link ~/result
@@ -16,9 +16,9 @@ echo "Activating configuration..."
 ~/result/activate
 
 echo "Linking zsh binary to /bin/zsh..."
-sudo ln -sf /home/zed/.nix-profile/bin/zsh /bin/zsh
+sudo ln -sf ~/.nix-profile/bin/zsh /bin/zsh
 
 echo "Changing default shell for user 'zed' to zsh..."
-sudo usermod -s /bin/zsh zed
+sudo usermod -s /bin/zsh $(whoami)
 
 echo "Setup complete."
